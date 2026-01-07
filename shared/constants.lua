e47922d2d2bfa3ff2f4cbd21ef56e9c6109133f5
Master Mind <48193931+iboss21@users.noreply.github.com>
2026-01-07 09:50:32 -0500

    Delete shared directory

--- shared/constants.lua
+++
@@ -1,6 +0,0 @@
-CONST = {}
-CONST.RISK={LOW='low',MEDIUM='medium',HIGH='high',EXTREME='extreme'}
-CONST.FW={AUTO='AUTO',LXR='LXR',RSG='RSG',VORP='VORP'}
-CONST.INTERACT={PROMPT='prompt',MURPHY='murphy',TARGET='ox_target'}
-function clamp(v,a,b) if v<a then return a elseif v>b then return b else return v end end
-function joaat(s) return GetHashKey(s) end
