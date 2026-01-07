9309812426ea3c1a5e91d5fafdd9400348ffb0a8
Master Mind <48193931+iboss21@users.noreply.github.com>
2026-01-07 09:50:40 -0500

    Delete sql directory

--- sql/lux_delivery.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `lux_delivery` (
-  `identifier` varchar(64) NOT NULL,
-  `level` int NOT NULL DEFAULT 0,
-  `xp` int NOT NULL DEFAULT 0,
-  PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
